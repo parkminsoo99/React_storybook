fc4fed7af90195e90d8315407c08ca237270f5f5
parkminsoo99 <zzangorc99@naver.com>
2020-12-16 13:55:27 +0000

    2020-12-16_add-jsx

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,12 @@
-import {configure} from '@storybook/react';
+import { configure, setAddon } from '@storybook/react';
 import interopRequireDefault from 'babel-runtime/helpers/interopRequireDefault';
+import JSXAddon from 'storybook-addon-jsx';
+
 function loadStories() {
 	const context = require.context('../src/stories', true, /Story\.jsx$/);
-	context.keys().ForEach((srcFile) => {interopRequireDefault(context(srcFile));});
+	context.keys().forEach((srcFile) => {interopRequireDefault(context(srcFile));});
 }
+setAddon(JSXAddon);
 configure(loadStories, module);
+
+
