7e5a56759062f0439e34b60c874c694309771a45
parkminsoo99 <zzangorc99@naver.com>
2020-12-17 14:40:09 +0000

    2020-12-17_react-with-styles

---
+++ src/04/Text.jsx
@@ -0,0 +1,19 @@
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+
+class Text extends PureComponent {
+	render(){
+		const{children} = this.props;
+		return (
+		<span>
+			{children}
+		</span>
+		);
+	}
+}
+
+Text.propTypes = {
+	children : PropTypes.node.isRequired,
+};
+
+export default Text;
