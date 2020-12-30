9e532b36273c84d918bfb3da5e53086be8f12c6e
parkminsoo99 <zzangorc99@naver.com>
2020-12-30 14:53:20 +0000

    2020-12-30_compose_lifecycle

--- src/stories/LifecycleStory.jsx
+++ src/stories/LifecycleStory.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 
-import {PageWithLoadData} from '../05/lifecycle';
+import {PageWithLoadData, PageWithLoadDataAndLoading} from '../05/lifecycle';
 
-storiesOf('LifeCycle', module).addWithJSX('loadData예제', () => (<PageWithLoadData loadData={() => fetch('/').then(() => 'hello')}/>));
+storiesOf('LifeCycle', module).addWithJSX('loadData예제', () => (<PageWithLoadData loadData={() => fetch('/').then(() => 'hello')}/>))
+.addWithJSX('로딩 메시지 예제', ()=>(<PageWithLoadDataAndLoading loadData={()=>fetch('/').then(() => 'hello')}/>));
