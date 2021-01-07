15f824019badaef9c7bafcd2b331841801b5c0bf
parkminsoo99 <zzangorc99@naver.com>
2021-01-07 14:35:20 +0000

    2020-01-07 공급자 & 소비자

--- src/05/test.jsx
+++ src/05/test.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import LoadingProvider from './LoadingProvider';
+import ButtonWithLoadingContext from './ButtonWithLoadingContext';
+import Button from '../04/Button';
 
-export default (loadingMessage = '로딩중') => WrappedComponent => {
-  const { displayName, name: componentName } = WrappedComponent;
-  const wrappedComponentName = displayName || componentName;
+function RowBComponent() {
+  return <Button>버튼</Button>;
+}
 
-  function WithLoading({ isLoading, ...props }) {
-    if (isLoading) {
-      return loadingMessage;
-    }
+function RowCComponent() {
+  return <ButtonWithLoadingContext label="버튼" />;
+}
 
-    return <WrappedComponent {...props} />;
+function TableComponent() {
+  return (
+    <table>
+      <RowBComponent />
+      <RowCComponent />
+    </table>
+  );
+}
+class HomePageComponent extends PureComponent {
+  render() {
+    return (
+      <LoadingProvider>
+        <TableComponent />
+        <ButtonWithLoadingContext label="상태 변경" />
+      </LoadingProvider>
+    );
   }
-  WithLoading.displayName = `withLoading(${wrappedComponentName})`;
-  return WithLoading;
-};
+}
+
+export default HomePageComponent;
--- src/06/LoadingProvider.jsx
+++ src/06/LoadingProvider.jsx
@@ -1,32 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class LoadingProvider extends React.Component{
-	constructor(props){
-		super(props);
-		
-		this.state={loading : false};
-		this.setLoading = this.setLoading.bind(this);
-	}
-	
-	getChildContext() {
-		return {
-			loading : this.state.loading,
-			setLoading : this.setLoading,
-		};
-	}
-	setLoading(loading){
-		this.setState({loading});
-	}
-	
-	render() {
-		return this.props.children;
-	}
+class LoadingProvider extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { loading: false };
+    this.setLoading = this.setLoading.bind(this);
+  }
+
+  getChildContext() {
+    return {
+      loading: this.state.loading,
+      setLoading: this.setLoading,
+    };
+  }
+
+  setLoading(loading) {
+    this.setState({ loading });
+  }
+
+  render() {
+    return this.props.children;
+  }
 }
 
-LoadingProvider.chileContextTypes ={
-	loading : PropTypes.bool,
-	setLoading : PropTypes.func,
+LoadingProvider.childContextTypes = {
+  loading: PropTypes.bool,
+  setLoading: PropTypes.func,
 };
 
 export default LoadingProvider;
